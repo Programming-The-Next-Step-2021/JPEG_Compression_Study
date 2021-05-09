67fdba9072932a8af8d5eae94e1c99e0b63b9a9e
JuliusMaerz <77966109+JuliusMaerz@users.noreply.github.com>
2021-05-09 16:53:45 +0200

    Update Compression_code.py

--- Compression_code.py
+++ Compression_code.py
@@ -1,1 +1,27 @@
 #This is going to be the code for the JPEG compression
+
+
+from PIL import Image
+import PIL
+import os
+import glob
+dir(Image)
+#print(dir(Image))
+
+im = Image.open("P4112976.JPG")
+print(f"The image size dimensions are: {im.size}")
+im.show()
+
+file_name = "image-1-compressed.jpg"
+dim = im.size
+print(f"this is the current size of the picture: {dim}")
+
+im.save("Compressed_"+file_name, optimize = True, quality=30)
+
+im2 = Image.open("Compressed_image-1-compressed.jpg")
+im2.show()
+
+
+
+
+
