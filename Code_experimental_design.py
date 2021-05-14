d07a9c73e4490aaf78be68aff07478dffbf205f3
JuliusMaerz <77966109+JuliusMaerz@users.noreply.github.com>
2021-05-14 21:33:35 +0200

    Update Code_experimental_design.py

--- Code_experimental_design.py
+++ Code_experimental_design.py
@@ -1,26 +1,48 @@
 """This package contains the code for the experimental design. It includes all trials step by step.
 It only works if you combine it with the contents of the compression-code.py package (some of which are imported into this package)."""
 from psychopy import visual, core, event
+from PIL import Image
+import PIL
+import os
+import glob
+import random
 
+from Stock_files_library import image_list
+import Compression_code
 
 #Create a window
-mywin = visual.Window([800,600], monitor = "testMonitor", units = "deg")
+mywin = visual.Window([1200,900], monitor = "testMonitor", units = "deg")
 """ This is the window in which the entire experiment will take place """
-help(mywin)
+#help(mywin)
 #Create stimulus
-stimulus = visual.GratingStim(win=mywin, mask="circle", size = 0.5, pos=[0,0], sf=0, rgb=-1)
-"""These are the general stimuli that the experiment will contain"""
-#stimulus.draw()
-#mywin.update()
-#core.wait(3.0)
-while True:
-    stimulus.setPhase(0.05, "+")
-    stimulus.draw()
-    mywin.flip()
 
-    if len(event.getKeys())>0:
-        break
-    event.clearEvents()
+uncompressed = random.choice(image_list)
+compressed = uncompressed
+
+stimulus = visual.ImageStim(win=mywin,
+                              image= uncompressed,
+                              size = (16, 12),
+                              pos=[-9,0],
+                            texRes = 1280,
+                            interpolate = True)
+stimulus2 = visual.ImageStim(win=mywin,
+                              image= compressed,
+                              size = (16, 12), pos=[9,0],
+                             texRes = 12)
+
+"""These are the general stimuli that the experiment will contain"""
+stimulus.draw()
+stimulus2.draw()
+mywin.flip()
+core.wait(5.0)
+#while True:
+#    stimulus.setPhase(0.05, "+")
+#    stimulus.draw()
+#    mywin.flip()
+#
+#    if len(event.getKeys())>0:
+#        break
+#    event.clearEvents()
 
 
 mywin.close()
