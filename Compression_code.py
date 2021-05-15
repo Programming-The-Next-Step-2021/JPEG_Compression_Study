4c59201a88f57d36f5166b003b9d5e6ec35f7a7b
JuliusMaerz <77966109+JuliusMaerz@users.noreply.github.com>
2021-05-15 09:35:52 +0200

    Update Compression_code.py

--- Compression_code.py
+++ Compression_code.py
@@ -1,5 +1,6 @@
 """This code deals with compressing JPEGs. It´s currently in an early state
-but in the end this package alone will be able to randomly compress JPEG files to any required size."""
+but in the end this package alone will be able to randomly compress JPEG files to any required size.
+All the images that are needed aere stored in the Stock_files_library file."""
 
 from psychopy import visual, core, event
 from PIL import Image
@@ -15,7 +16,7 @@ dir(Image)
 
 im = Image.open(random.choice(image_list))
 print(f"The image size dimensions are: {im.size}")
-#im.show()
+
 
 file_name = "image-1-compressed.jpg"
 dim = im.size
@@ -25,7 +26,7 @@ im.save("Compressed_"+file_name, optimize = True, quality=30)
 """This line of code compresses the JPEGs to 30% quality. More details will follow"""
 
 im2 = Image.open("Compressed_image-1-compressed.jpg")
-#im2.show()
+
 
 
 
