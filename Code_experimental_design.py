04cf0ce9a967897d2d15dcd02654bab535c104c7
JuliusMaerz <77966109+JuliusMaerz@users.noreply.github.com>
2021-05-19 11:48:17 +0200

    Update Code_experimental_design.py

--- Code_experimental_design.py
+++ Code_experimental_design.py
@@ -15,6 +15,7 @@ import Compression_code
 
 #General issues
 clock = Clock()
+kb = Keyboard()
 
 #Create a window
 mywin = visual.Window([1200,900], monitor = "testMonitor", units = "deg")
@@ -22,35 +23,63 @@ mywin = visual.Window([1200,900], monitor = "testMonitor", units = "deg")
 #help(mywin)
 #Create stimulus
 
-uncompressed = random.choice(image_list)
-compressed = uncompressed
-
-stimulus = visual.ImageStim(win=mywin,
-                              image= uncompressed,
-                              size = (16, 12),
-                              pos=[-9,0],
-                            texRes = 1280,
-                            interpolate = True)
-stimulus2 = visual.ImageStim(win=mywin,
-                              image= compressed,
-                              size = (16, 12), pos=[9,0],
-                             texRes = 12)
-
 """These are the general stimuli that the experiment will contain"""
-stimulus.draw()
-stimulus2.draw()
+
+#Introduction text
+introduction_txt = visual.TextStim(win = mywin, text= "Hello! Welcome to the experiment. Press space to continue")
+introduction_txt.draw()
 mywin.flip()
-core.wait(5.0)
-#while True:
-#    stimulus.setPhase(0.05, "+")
-#    stimulus.draw()
-#    mywin.flip()
-#
-#    if len(event.getKeys())>0:
-#        break
-#    event.clearEvents()
+while True:
+        keys = kb.getKeys()
+        if "space" in keys:
+            break
 
+explanation_txt = visual.TextStim(win = mywin, text = """In this simple visual reaction task, you will have to repeatedly distinguish between two pictures
+Both pictures will show the same motive but one of them will be in a lower resolution than the other.
+Your task will be to recognize the LOWER resolution picture as quickly as possible.
+Press space to continue""")
+explanation_txt.draw()
+mywin.flip()
+while True:
+        keys = kb.getKeys()
+        if "space" in keys:
+            break
 
+start_txt = visual.TextStim(win = mywin, text = """You can confirm your choice by pressing one of two buttons.
+Press "f" if you think the picture on the LEFT is in LOWER resolution.
+Press "j" if you think the picture on the RIGHT is in LOWER resolution.
+Please try to react as quickly and as correctly as possible.
+Press space to begin.""")
+start_txt.draw()
+mywin.flip()
+while True:
+        keys = kb.getKeys()
+        if "space" in keys:
+            break
+
+#Trials                       
+def trial():
+    picture_1 = random.choice(image_list)
+    picture_2 = random.choice(image_list)
+    left = visual.ImageStim(win=mywin,image= picture_1,size = (16, 12),pos=[-9,0],texRes = 1280,interpolate = True)
+    right = visual.ImageStim(win=mywin,image= picture_1,size = (16, 12), pos=[9,0], texRes = 12)
+    instruction = visual.TextStim(win = mywin, text = """Which picture is in lower resolution?
+Press "f" for the left picture.
+Press "j" for the right picture.""", pos = [0, 10], color = (1.0, 1.0))
+    left.draw()
+    right.draw()
+    instruction.draw()
+    mywin.flip()
+    while True:
+        keys = kb.getKeys()
+        if "f" in keys:
+            break
+        elif "j" in keys:
+            break
+
+for i in range(20):
+    trial()
+    
 mywin.close()
 core.quit()
 
