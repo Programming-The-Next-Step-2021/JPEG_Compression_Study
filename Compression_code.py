06d65b73a5be4930ad69a34b88df40e0ada64a54
JuliusMaerz <77966109+JuliusMaerz@users.noreply.github.com>
2021-05-19 11:48:20 +0200

    Update Compression_code.py

--- Compression_code.py
+++ Compression_code.py
@@ -15,14 +15,14 @@ dir(Image)
 #print(dir(Image))
 
 im = Image.open(random.choice(image_list))
-print(f"The image size dimensions are: {im.size}")
+#print(f"The image size dimensions are: {im.size}")
 
 
 file_name = "image-1-compressed.jpg"
 dim = im.size
-print(f"this is the current size of the picture: {dim}")
+#print(f"this is the current size of the picture: {dim}")
 
-im.save("Compressed_"+file_name, optimize = True, quality=30)
+#im.save("Compressed_"+file_name, optimize = True, quality=30)
 """This line of code compresses the JPEGs to 30% quality. More details will follow"""
 
 im2 = Image.open("Compressed_image-1-compressed.jpg")
