66550b7a9ce374f4076e1453172c7d40c6cb3f25
JuliusMaerz <77966109+JuliusMaerz@users.noreply.github.com>
2021-05-30 19:08:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setuptools.setup(
   name = "jpeg_compression",
   version = "0.0.1",
   author = "Julius",
-  author_email = "julius.marz@student.uva.nl"
+  author_email = "julius.marz@student.uva.nl",
   python_requires = ">=3.6",
   packages = setuptools.find_packages()
 )
